2805f16440418c89c46a452e25c10c37b9e80670
Trevor Fairbank <Trevorfairbank92@gmail.com>
2019-07-03 14:41:21 -0700

    understanding handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Chicken Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Ground Pork Burger", false);
